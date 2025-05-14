d93452b2412a604b497fb77041b4de558afa2455
effazrayhan <bsse1501@iit.du.ac.bd>
2025-05-14 11:26:01 +0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.iit.ecomm.ecomm {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.iit.ecomm.ecomm to javafx.fxml;
+    exports com.iit.ecomm.ecomm;
+}
